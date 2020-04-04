f1ac83062d79010da520334f4c6b717944950be0
Abdelrahman HSN <abdelrahmanhsn1@gmail.com>
2020-04-04 23:24:16 +0200

    Fix Codacy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-20.times do |i|
+20.times do
   Driver.create(
     email: Faker::Internet.email,
     name: Faker::Name.name ,
